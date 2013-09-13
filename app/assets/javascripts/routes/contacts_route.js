0c236ccf59dff92289975750c8661e89feb6985c
CodeOfficer <codeofficer@gmail.com>
2013-09-13 02:07:25 -0400

    find and filter are now called directly on the store

--- app/assets/javascripts/routes/contacts_route.js
+++ app/assets/javascripts/routes/contacts_route.js
@@ -1,10 +1,10 @@
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
     // request all contacts from adapter
-    App.Contact.find();
+    this.store.find('contact');
 
     // filter contacts to exclude new ones
-    return App.Contact.filter(function(contact) {
+    return this.store.filter('contact', function(contact) {
       return !contact.get('isNew');
     });
   }
