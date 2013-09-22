7fb7a47d38ccb69c88baa183024e68753699499a
CodeOfficer <codeofficer@gmail.com>
2013-09-22 16:09:45 -0400

    re-implement stopEditing

--- app/assets/javascripts/controllers/contacts_new_controller.js
+++ app/assets/javascripts/controllers/contacts_new_controller.js
@@ -29,17 +29,6 @@ App.ContactsNewController = Em.ObjectController.extend({
   },
 
   stopEditing: function() {
-    // rollback the local transaction if it hasn't already been cleared
-    if (this.transaction) {
-      this.transaction.rollback();
-      this.transaction = null;
-    }
-  },
-
-  transitionAfterSave: function() {
-    // when creating new records, it's necessary to wait for the record to be assigned
-    // an id before we can transition to its route (which depends on its id)
-    if (this.get('model.id')) {
-      this.transitionToRoute('contact', this.get('model'));
-    }
+    this.get('model').rollback();
+  }
 });
